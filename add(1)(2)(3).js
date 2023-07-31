7641e3fcf1f677414ec1f7b86c9bc683bde0fc9b
barrygao(高聪) <barrygao@tencent.com>
2023-07-31 17:03:51 +0800

    feat: 修改add函数

--- add(1)(2)(3).js
+++ add(1)(2)(3).js
@@ -1,12 +1,24 @@
-function add(x){
-    var sum = function(y){
-        x = x + y;
-        return sum
-    }
+/**
+请实现一个 add 函数，满足以下功能 
+add(1); 	// 1
+add(1)(2);  	// 3
+add(1)(2)(3)；  // 6
+add(1)(2, 3);   // 6
+add(1, 2)(3);   // 6
+add(1, 2, 3);   // 6
 
-    sum.toString = function(){
-        return x
-    }
+*/
 
-    return sum;
+function add() {
+  let args = [...arguments];
+  let addfun = function () {
+    args.push(...arguments);
+    return addfun;
+  };
+  addfun.toString = function () {
+    return args.reduce((a, b) => {
+      return a + b;
+    });
+  };
+  return addfun;
 }
